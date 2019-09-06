8e6a7369d8a3fda33356e846436af192d9d83b49
avinash.kademani <avinash.kademani@accenture.com>
2019-09-06 15:04:15 +0530

    done

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -30,8 +30,8 @@ public class HelloTest {
 	public void f() {
 
 		//text = driver.findElement(By.xpath("html/body/h2"));
-		//Assert.assertEquals(driver.getTitle(), "");
-		//System.out.println("Test Passed");
+		Assert.assertEquals(driver.getTitle(), "Hello World");
+		System.out.println("Test Passed");
 
 	}
 
